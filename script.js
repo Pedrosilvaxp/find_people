e145b9fbb933861dd3db1389f781c8d4824ba770
Pedro Silva do Nascimento <phpedrojs@gmail.com>
2024-11-26 12:13:09 -0300

    Adicionando novas funcionalidades
    Pesquisa por nome, idade e nacionalidade.
    
    Adicionei tratamento de erros na requisição da API, comunicando ao usuário, caso houver algum erro.

--- script.js
+++ script.js
@@ -1,69 +1,123 @@
 const section = document.querySelector('section');
 const container = document.querySelector('.container');
 const formulario = document.querySelector('form');
-const paragrafo = document.querySelector('p');
+const mensagem = document.querySelector('p');
 const circleLoader = document.querySelector('#iconLoader');
 
-
-async function getPeople() {
-    const response = await fetch("https://randomuser.me/api/?inc=name,phone,location,dob,picture&nat=br&results=4000");
-
-    return response.json();
-}
 
 formulario.addEventListener('submit', async (event) => {
     event.preventDefault(); // Evitando que os dados sejam submetidos
 
-    paragrafo.style.opacity = '0';
+    mensagem.style.opacity = '0';
     circleLoader.style.opacity = '1';
 
-    const data = await getPeople();
-    const pessoas = data.results;
-
+    // Processamento
     const nomeBusca = document.querySelector('#nome').value;
-    // Buscando nome
-    const pessoaEncontrada = pessoas.find(pessoa => pessoa.name.first.toLowerCase() === nomeBusca.toLowerCase() || pessoa.name.last.toLowerCase() === nomeBusca.toLowerCase());
+    const idadeBusca = document.querySelector('#idade').value;
+    const paisSelecionado = document.querySelector('select#escolha').value;
 
-    circleLoader.style.opacity = '0';
-    paragrafo.style.opacity = '1';
 
-    if (!pessoaEncontrada) {
-        paragrafo.innerHTML = "<p style='color: #24242475;'>Sem resultados. Busque novamente</p>";
+    try {
+		// Fazendo a requisição com o parâmetro de consulta correspondente ao país selecionado pelo usuário
+        const response = await fetch(`https://randomuser.me/api/?inc=name,phone,location,dob,picture&results=4000&nat=${paisSelecionado}`);
 
-    } else {
-        paragrafo.innerHTML = "<p style='color: #28B5D4;'>Pessoa encontrada!</p>";
+        const data = await response.json();
 
-        const novaPessoa = document.createElement('div');
-        novaPessoa.classList.add('novaPessoa');
+        const pessoas = data.results;
 
-        const nome = document.createElement('p');
-        nome.textContent = `Nome: ${pessoaEncontrada.name.first} ${pessoaEncontrada.name.last}`;
+        // Buscando pessoas
+        let pessoasEncontradas = pessoas;
 
-        const pais = document.createElement('p');
-        pais.textContent = `País: ${pessoaEncontrada.location.country}`;
+        if (idadeBusca == '' && nomeBusca != '') {
+            // Pesquisa apenas por nome
+            pessoasEncontradas = pessoas.filter(pessoa => pessoa.name.first.toLowerCase() == nomeBusca.toLowerCase() || pessoa.name.last.toLowerCase() == nomeBusca.toLowerCase());
 
-        const idade = document.createElement('p');
-        idade.textContent = `Idade: ${pessoaEncontrada.dob.age}`;
+        } else if (idadeBusca != '' && nomeBusca == '') {
+            // Por idade
+            pessoasEncontradas = pessoas.filter(pessoa => pessoa.dob.age == idadeBusca);
 
-        const phone = document.createElement('p');
-        phone.textContent = `Telefone: ${pessoaEncontrada.phone}`;
+        } else if (idadeBusca != '' && nomeBusca != '') {
+            // Conforme nome e idade
+            pessoasEncontradas = pessoas.filter(pessoa => (pessoa.name.first.toLowerCase() == nomeBusca.toLowerCase() || pessoa.name.last.toLowerCase() == nomeBusca.toLowerCase()) && pessoa.dob.age == idadeBusca);
 
-        // Gerando foto
-        const foto = document.createElement('img');
-        foto.setAttribute('src', `${pessoaEncontrada.picture.large}`);
-        foto.classList.add('foto');
+        } else {
+            // Apenas por nacionalidade
+            // Adicionando apenas 50, ao invés de todo resultado da requisição
+            let resultadoPessoas = [];
 
-        // Atribuindo informações
-        novaPessoa.append(foto, nome, pais, idade, phone);
+            for (let c = 0; c < 50; c++) {
+                resultadoPessoas.push(pessoasEncontradas[c]);
+            }
+            pessoasEncontradas = resultadoPessoas;
+        }
+ 
+        circleLoader.style.opacity = '0';
+        mensagem.style.opacity = '1';
 
-        novaPessoa.style.border = '2px solid #28B5D4';
-        setInterval(() => {
-            novaPessoa.style.border = '0px solid';
-        }, 3000);
+        if (pessoasEncontradas.length < 1) {
+            mensagem.innerHTML = "<p style='color: #24242475;'>Sem resultados. Busque novamente.</p>";
 
-        // Exibindo informações
-        container.appendChild(novaPessoa);
-        
-        section.style.height = 'auto';
+        } else {
+            if (pessoasEncontradas.length > 1) {
+                mensagem.innerHTML = `<p style='color: #28B5D4;'> ${pessoasEncontradas.length} pessoas encontradas!</p>`;
+            } else {
+                mensagem.innerHTML = "<p style='color: #28B5D4;'>Uma pessoa encontrada!</p>";
+            }
+
+            pessoasEncontradas.map(pessoaEncontrada => {
+
+                // Gerando dados
+                const novaPessoa = document.createElement('div');
+                novaPessoa.classList.add('novaPessoa');
+
+                const nome = document.createElement('p');
+                nome.textContent = `Nome: ${pessoaEncontrada.name.first} ${pessoaEncontrada.name.last}`;
+
+                const pais = document.createElement('p');
+                pais.textContent = `País: ${pessoaEncontrada.location.country}`;
+
+                const idade = document.createElement('p');
+                idade.textContent = `Idade: ${pessoaEncontrada.dob.age}`;
+
+                const phone = document.createElement('p');
+                phone.textContent = `Telefone: ${pessoaEncontrada.phone}`;
+
+                // Gerando foto
+                const foto = document.createElement('img');
+                foto.setAttribute('src', `${pessoaEncontrada.picture.large}`);
+                foto.classList.add('foto');
+
+                // Atribuindo informações
+                novaPessoa.append(foto, nome, pais, idade, phone);
+
+                novaPessoa.style.border = '2px solid #28B5D4';
+                setInterval(() => {
+                    novaPessoa.style.border = '0px solid';
+                }, 3000);
+
+                // Exibindo informações
+                container.appendChild(novaPessoa);
+
+                section.style.height = 'auto';
+            });
+        }
+
+    } catch (error) {
+        console.error(error);
+        mensagem.innerHTML = "<p style='color: red;'>Ocorreu um erro ao realizar a busca. <p style='color: #28B5D4;'>Atualize a página.</p></p>"
     }
+});
+
+
+// Botão de voltar
+const button = document.querySelector('#backBtn');
+
+window.onscroll = () => {
+	const userScroll = document.documentElement.scrollTop;
+
+	button.style.opacity = userScroll > 200 ? '1' : '0';
+};
+
+button.addEventListener('click', () => {
+	scrollTo({top: 0, behavior:'smooth'});
 });
